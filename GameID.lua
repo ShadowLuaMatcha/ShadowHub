a7f5c8db3482f7b6f525bc4368e7da7ad074baea
WhyMayko <128808705+WhyMayko@users.noreply.github.com>
2026-03-16 21:18:12 -0300

    Update GameID.lua

--- GameID.lua
+++ GameID.lua
@@ -1,6 +1,6 @@
 ShadowLuaGames = {
     {
         ids = { "11392373641", "128490927897972", "98587275685649", "125998488582708", "78840529469877", "76758897829889" },
-        script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/WhyMayko/ShadowHub/refs/heads/main/Scripts/Eternal%20Nights.lua"))()]]
+        script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/ShadowLuaMatcha/ShadowHub/refs/heads/main/Scripts/EternalNights.lua"))()]]
     },
 }
